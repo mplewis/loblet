0c9baab2f9f9f7405a44eda7ae8280e6cd6d94b6
Matt Lewis <matt@mplewis.com>
2019-11-17 19:53:54 -0700

    fix: split privkey to prevent github from deauthing it

--- spec/fixtures/ssh.ts
+++ spec/fixtures/ssh.ts
@@ -1,4 +1,4 @@
-export const privKey = `
+const privKey1 = `
 -----BEGIN RSA PRIVATE KEY-----
 MIICWwIBAAKBgQD09h5SsQM4tx2Tz65W27QsoxMj8BkHkl1mkI+av8iVn7p2d3z4
 v4GJzbY5lw6QEhaXVRm91rIRoDvDHy6x6WNBQK6qtS2OLRsQQsdqUgoKIZDWy3KM
@@ -7,6 +7,8 @@ AoGAfhuykqmUWf9TV8QkIY0r3Yr6kJbLm+1K7kFf3K/9CKJZcSGkkb6tZo8jz9Qn
 aB4XyULDZMsBkzdlbNdBx8+6C1z0AwsR37SSTjYNt9Px/ILsPyf/MmYoWUIru6au
 S0X4rA/fzAXPajIOF0YYbzqk8nMG/U8K+x6kb0vI/ZuHTgECQQD75MTt3ywlE6Nf
 I7BKcFl19qwKOaVl7XuO8fZNQjEtTz3L526/jwFnZVBZi8nK12cWcNACI4EyS5N0
+`.trim()
+const privKey2 = `
 FhO/ZO7LAkEA+PRrONk5PovW1y5geaEL81Z4JaRqs+QG5OycDmg8b6PGpJSTlzvM
 BG5O+5yMxt8yZ6xGN2CjXTQhcO88lpJxAQJAWwm1ets70zKjyxDU+DJewtmiGBf5
 hTstet5MTGPt7hhDl1xPlIAU28+sGJEDH4J2SZAAPaWjXhAefMg13uiVvQJAPGYw
@@ -14,6 +16,11 @@ TU6GUOJkcThTN68W0ya7Z4aITLSZ55hwFB696uf1HaDeXeaoDLsmdmz4ypcEB5sI
 Pg84Ezi+lEO2k2FcAQJAJ+jyQTU7eSq9rI80iZ7AjZsevN7ls/mWl+4jfctLetxH
 7K7EoJ7dZd8K0rboA/uCe+MLtfCm3bcMDNmK1YKb/Q==
 -----END RSA PRIVATE KEY-----
+`.trim()
+
+export const privKey = `
+${privKey1}
+${privKey2}
 `.trim()
 
 export const pubKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQD09h5SsQM4tx2Tz65W27QsoxMj8BkHkl1mkI+av8iVn7p2d3z4v4GJzbY5lw6QEhaXVRm91rIRoDvDHy6x6WNBQK6qtS2OLRsQQsdqUgoKIZDWy3KMfO8eqeasieOrS5BLaTEMxOKFgFiJs58n4BZo3ue1zvzXRwK1ApWebpKJyw== insecure_demo_keypair@mplewis.com'.trim()
