d3a4f1db40b0f3b5769f0e1014589d9f429524ae
Matt Lewis <matt@mplewis.com>
2019-11-17 19:38:37 -0700

    fix: tempdir works with non empty

--- spec/utils/tempdir.ts
+++ spec/utils/tempdir.ts
@@ -1,10 +1,11 @@
 import * as tmp from 'tmp-promise'
+import { rmdir } from 'fs-extra'
 
 export default async function tempdir (fn: (dir: string) => Promise<void>): Promise<void> {
-  const { path, cleanup } = await tmp.dir()
+  const { path } = await tmp.dir()
   try {
     await fn(path)
   } finally {
-    await cleanup()
+    await rmdir(path)
   }
 }
