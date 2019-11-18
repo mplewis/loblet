d83f1f54eb58fbf6c1d2b93e5064964ea88383ce
Matt Lewis <matt@mplewis.com>
2019-11-17 19:38:12 -0700

    refactor: extract gh known host

--- src/ssh.spec.ts
+++ src/ssh.spec.ts
@@ -1,6 +1,6 @@
 import { ensureKnownHost, KNOWN_HOSTS_FILE } from './ssh'
 import ensureContainerized from '../spec/utils/ensure_containerized'
-import { privKey } from '../spec/fixtures/ssh'
+import { privKey, githubKnownHost } from '../spec/fixtures/ssh'
 import { unlink, ensureFile, writeFile } from 'fs-extra'
 import { read } from './fs'
 import { exec } from './shell'
@@ -9,12 +9,11 @@ import expandTilde = require('expand-tilde')
 ensureContainerized()
 
 const ID_RSA = expandTilde('~/.ssh/id_rsa')
-const GITHUB_KNOWN_HOST = 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=='
 
 async function itPopulatesTheFileAsExpected (times = 1): Promise<void> {
   it('populates the file as expected', async () => {
     for (let i = 0; i < times; i++) {
-      await ensureKnownHost(GITHUB_KNOWN_HOST)
+      await ensureKnownHost(githubKnownHost)
     }
     expect(await read(KNOWN_HOSTS_FILE)).toMatchSnapshot()
   })
